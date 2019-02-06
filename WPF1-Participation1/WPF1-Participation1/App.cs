0fed2caf70bf43c976785111fa614cab3bf07ac1
harr1626 <harr1626@PCB-LAB360-24>
2019-02-06 11:35:58 -0600

    Creation of in class participation 1 2/6/2019

---
+++ WPF1-Participation1/WPF1-Participation1/App.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF1_Participation1
+{
+    class Information
+    {
+        public string Name { get; set; }
+        public string Address { set; get; }
+        public int ZipCode { set; get; }
+
+        public Information()
+        {
+            Name = "";
+            Address = "";
+            ZipCode = 0;
+        }
+    }
+}
---
+++ WPF1-Participation1/WPF1-Participation1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF1_Participation1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            Information a = new Information();
+            string zipAsString = txtZipCode.Text;
+            a.Name = txtName.Text;
+            a.Address = txtAddress.Text;
+            a.ZipCode = Convert.ToInt32(zipAsString);
+            lstEntry.Items.Add(a);
+            
+        }
+
+       
+    }
+}
